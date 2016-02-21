c80ac1227fbe04462eb179d2ae508d9494122a4a
lechoPl <lechopl@gmail.com>
2016-02-21 22:02:38 +0100

    Init commit

---
+++ PrototypeGameUnity/Assets/Scripts/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public float Speed = 6f;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        transform.Rotate(new Vector3(Speed * Time.deltaTime, 0, 0));
+	}
+}
