d4db0973b1281ced5c83058760c8978f5c544b78
lechoPl <lechopl@gmail.com>
2016-02-26 12:45:10 +0100

    Add simple player controller

---
+++ PrototypeGameUnity/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform player;
+    public float smoothingX = 5f;
+    public float smoothingY = 5f;
+
+    private Vector3 offset;
+
+    // MonoBehavior methods
+    //***********************************
+    void Start()
+    {
+        offset = transform.position - player.position;
+    }
+
+    void FixedUpdate()
+    {
+        var targetCamPos = player.position + offset;
+
+        transform.position = new Vector3(
+            Mathf.Lerp(transform.position.x, targetCamPos.x, smoothingX),
+            Mathf.Lerp(transform.position.y, targetCamPos.y, smoothingY),
+            targetCamPos.z);
+    }
+}
---
+++ PrototypeGameUnity/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Physics")]
+    public float Speed = 5f;
+    public float jumpForce = 700f;
+    public int maxJumpCount = 2;
+
+    [Header("Ground Detection")]
+    public Transform GroundCheck;
+    public float GroundCheckRadius = 0.01f;
+    public LayerMask GroundMask;
+
+    private Rigidbody2D _rigidbody;
+    private bool isGrounded = false;
+    private int jumpCount = 0;
+
+
+    // MonoBehavior methods
+    //***********************************
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, GroundMask);
+        if (isGrounded)
+        {
+            jumpCount = 0;
+        }
+
+
+        float move = Input.GetAxis("Horizontal");
+        //float jump = Input.GetKey("Jump");
+
+        _rigidbody.velocity = new Vector2(move * Speed, _rigidbody.velocity.y);
+    }
+
+    void Update()
+    {
+        if ((isGrounded || jumpCount < maxJumpCount-1) && Input.GetButtonDown("Jump"))
+        {
+            isGrounded = false;
+            ++jumpCount;
+            _rigidbody.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+}
