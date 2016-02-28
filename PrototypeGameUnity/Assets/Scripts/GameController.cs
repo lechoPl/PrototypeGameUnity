3f1d77812ba15c6a3b6578e3132276291ac34956
lechoPl <lechopl@gmail.com>
2016-02-28 22:16:47 +0100

    Add simple game controller

---
+++ PrototypeGameUnity/Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    [Header("UI")]
+    public GameObject GameMenuPanel;
+    public GameObject MoveUI;
+
+
+    // MonoBehavior methods
+    //***********************************
+    void Update ()
+    {
+        CheckInput();
+	}
+
+    // Private methods
+    //***********************************
+    private void CheckInput()
+    {
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            SwitchUI();
+        }
+    }
+
+    private void SwitchUI()
+    {
+        if(GameMenuPanel != null)
+        {
+            GameMenuPanel.SetActive(!GameMenuPanel.activeSelf);
+        }
+
+        if(MoveUI != null)
+        {
+            MoveUI.SetActive(!GameMenuPanel.activeSelf);
+        }
+    }
+}
